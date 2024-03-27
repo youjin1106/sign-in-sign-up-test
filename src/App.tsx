6424dea2613bc3106509f52567d94bc184187786
Jang YouJin <jjnote22@gmail.com>
2024-03-27 10:11:38 +0900

    로그인 폼 마크업 FormData로 로그인정보 가져옴

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,29 @@
+import { FormEventHandler } from "react";
 import "./index.css";
 
 function App() {
-  return <div className="bg-cyan-600">test</div>;
+  const formAction: FormEventHandler<HTMLFormElement> = (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const email = formData.get("email") as string;
+    const password = formData.get("password") as string;
+    signInWithEmailAndPassword(email, password);
+  };
+
+  function signInWithEmailAndPassword(email, password) {
+    console.log(email, password);
+  }
+  return (
+    <div className="flex flex-col bg-slate-200 w-[400px] m-10">
+      <div className="text-lg text-center">sign in</div>
+      <form onSubmit={formAction} className="flex flex-col p-5">
+        <input name="email" type="text" />
+        <br />
+        <input name="password" type="text" />
+        <input type="submit" value="submit" />
+      </form>
+    </div>
+  );
 }
 
 export default App;
